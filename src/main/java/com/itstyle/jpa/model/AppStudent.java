77362318cf2bea99d37d57e52526d47a3e6a54f6
zzh-1992 <ezzh1992@icloud.com>
2021-09-01 20:16:32 +0800

    使用lombok注解替换getter、setter

--- src/main/java/com/itstyle/jpa/model/AppStudent.java
+++ src/main/java/com/itstyle/jpa/model/AppStudent.java
@@ -1,4 +1,8 @@
 package com.itstyle.jpa.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +12,9 @@ import javax.persistence.Table;
 //学生表
 @Entity
 @Table(name = "app_student")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppStudent {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,46 +23,11 @@ public class AppStudent {
 	private Integer classId;
 	private String name;
 	private Integer age;
-	
-	public AppStudent() {
-		super();
-	}
-	public AppStudent(Integer id, Integer classId, String name, Integer age) {
-		super();
-		this.id = id;
-		this.classId = classId;
-		this.name = name;
-		this.age = age;
-	}
+
 	public AppStudent(Integer classId, String name, Integer age) {
 		super();
 		this.classId = classId;
 		this.name = name;
 		this.age = age;
 	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public Integer getClassId() {
-		return classId;
-	}
-	public void setClassId(Integer classId) {
-		this.classId = classId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	
 }
--- src/main/java/com/itstyle/jpa/model/User.java
+++ src/main/java/com/itstyle/jpa/model/User.java
@@ -1,5 +1,9 @@
 package com.itstyle.jpa.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 import javax.persistence.Column;
@@ -16,6 +20,9 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "sys_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -26,36 +33,4 @@ public class User implements Serializable{
 	private String name;
 	@Column(nullable = false, name = "age")
 	private Integer age;
-
-	public User() {
-	}
-
-	public User(String name, Integer age) {
-		this.name = name;
-		this.age = age;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
 }
